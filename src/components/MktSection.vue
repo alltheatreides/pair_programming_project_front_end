e945a68c83e2a5fcd08413355b32c104a541c1af
Moninas <ana_monica_s@hotmail.com>
2022-03-21 19:23:38 +0100

    content afternoon work

--- src/components/MktSection.vue
+++ src/components/MktSection.vue
@@ -6,35 +6,41 @@ import Card from "./small/Card.vue";
 
 <template>
    <section
-      class="container mx-auto px-6 md:px-10 lg:px-0 before:block before:border-b-8 before:border-themeQuaternary before:w-4/6 md:before:w-2/6 lg:before:w-1/6 before:rounded-xl before:mb-6 mb-24 lg:mt-20"
+      class="container mx-auto px-6 md:px-10 lg:px-0 before:block before:border-b-8 before:border-themeQuaternary before:w-4/6 md:before:w-2/6 lg:before:w-1/6 before:rounded-xl before:mb-6 mb-24 md:mt-20"
    >
-      <h2 class="text-3xl mb-6 font-bold md:w-5/12 leading-relaxed">
-         Pourquoi mesurer son sommeil ?
-      </h2>
+      <article class="flex flex-col lg:flex-row">
+         <div class="w-3/6">
+            <h2
+               class="text-3xl mb-6 font-bold md:w-5/12 leading-relaxed"
+            >Pourquoi mesurer son sommeil ?</h2>
 
-      <p class="my-2 mt-4 lg:w-3/6">
-         Les avantages d'une bonne nuit de sommeil sont connus de tous, mais peu
-         de personnes dorment réellement huit heures par nuit voire plus.
-      </p>
-      <div
-         class="grid grid-cols-1 md:grid-cols-2 mt-6 gap-4 lg:gap-10 lg:w-3/6"
-      >
-         <Card
-            title="Améliorer la qualité du sommeil."
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            <p class="my-2 mt-4 lg:w-5/6">
+               Les avantages d'une bonne nuit de sommeil sont connus de tous, mais peu
+               de personnes dorment réellement huit heures par nuit voire plus.
+            </p>
+            <div
+               class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 lg:w-3/6 mt-6 gap-4 lg:gap-10"
+            >
+               <Card
+                  title="Améliorer la qualité du sommeil."
+                  description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+               />
+               <Card
+                  title="Améliorer la qualité du sommeil."
+                  description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+               />
+               <Card
+                  title="Améliorer la qualité du sommeil."
+                  description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+               />
+            </div>
+         </div>
+
+         <img
+            class="mt-8 w-3/6"
+            src="/public/pictures/sleep2.jpg"
+            alt="Photo by Lux Graves on Unsplash"
          />
-         <Card
-            title="Améliorer la qualité du sommeil."
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-         />
-         <Card
-            title="Améliorer la qualité du sommeil."
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-         />
-      </div>
-      <img
-         src="/public/pictures/photo-1531353826977-0941b4779a1c.avif"
-         alt=""
-      />
+      </article>
    </section>
 </template>
