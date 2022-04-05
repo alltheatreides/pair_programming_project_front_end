4be3d9f1b2358cf4e68d5713f0baeaea0fc37707
alltheatreides <goldschmidt.antoine@gmail.com>
2022-04-05 15:14:27 +0200

    Content: Tailwind Configuration Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      themePrimary: '#FCF9FC',
-      themeSecondary: '#111637',
-      themeTertiary: '#FCC93C',
-      themeQuaternary: '#3FA7D8',
-      themeQuaternaryDarker: '#3fa7d866',
-      themeSecondaryDarker: '#3fa7d81a'
+
+    extend: {
+      colors: {
+        themePrimary: '#FCF9FC',
+        themeSecondary: '#111637',
+        themeTertiary: '#FCC93C',
+        themeQuaternary: '#3FA7D8',
+        themeQuaternaryDarker: '#3fa7d866',
+        themeSecondaryDarker: '#3fa7d81a'
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
